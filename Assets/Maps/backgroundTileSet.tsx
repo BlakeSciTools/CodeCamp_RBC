dfdf367bedf7f0942de58606f1c2295a23a29c1f
robby <hell@hello.com>
2017-11-10 16:46:27 -0700

    New map testing.

---
+++ Assets/Maps/backgroundTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="topDownShooter" tilewidth="4" tileheight="4" tilecount="138240" columns="432">
+ <image source="topDownShooter/Tilesheet/tilesheet_complete.png" width="1728" height="1280"/>
+</tileset>
