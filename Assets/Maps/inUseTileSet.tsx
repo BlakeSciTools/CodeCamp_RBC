59ceb952210ed582794cddab46701986a55f1ab6
robby <hell@hello.com>
2017-11-10 21:27:57 -0700

    Final map for real test.

--- Assets/Maps/inUseTileSet.tsx
+++ Assets/Maps/inUseTileSet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="needPictures" tilewidth="8" tileheight="8" tilecount="500" columns="50">
- <image source="needMaps.png" width="400" height="84"/>
+<tileset name="finalTileSet" tilewidth="8" tileheight="8" tilecount="273" columns="21">
+ <image source="finalPicture.png" width="173" height="104"/>
 </tileset>
